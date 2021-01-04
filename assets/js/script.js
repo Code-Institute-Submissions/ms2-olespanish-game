da0629d9bb7a423fffc813c461f61153299229bb
fernandagil <fernandagilgonzalez@gmail.com>
2021-01-04 23:03:16 +0000

    Hid the front of the cards and added a flip over effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,22 +56,31 @@ gameGrid.sort(() => 0.5 - Math.random());
 
 // Grab the div with an id of root
 const game = document.getElementById('game');
-
 // Create a section with a class of grid
 const grid = document.createElement('section');
 grid.setAttribute('class', 'grid');
-
 // Append the grid section to the game div
 game.appendChild(grid);
 
 // For each item in the cardsArray array
-gameGrid.forEach(item => {
+gameGrid.forEach((item) => {
   const card = document.createElement('div'); // Create a div
   card.classList.add('card'); // Apply a card class to that div
   card.dataset.name = item.name; // Set the data-name attribute of the div to the cardsArray name
-  card.style.backgroundImage = `url(${item.img})`; // Apply the background image of the div to the cardsArray image
+  
+  // Create back of card
+  const back = document.createElement('div');
+  front.classList.add('back');
 
-  grid.appendChild(card); // Append the div to the grid section
+  // Create front of card, which contains
+  const front = document.createElement('div')
+  back.classList.add('front');
+  back.style.backgroundImage = `url(${item.img})`;
+
+  // Append card to grid, and front and back to each card
+  grid.appendChild(card);
+  card.appendChild(front);
+  card.appendChild(back);
 });
 
 // Add match CSS
@@ -98,7 +107,11 @@ grid.addEventListener('click', function (event) {
   let clicked = event.target; // The event target is our clicked item
 
   // Do not allow the grid section itself to be selected; only select divs inside the grid
-  if (clicked.nodeName === 'SECTION' || clicked === previousTarget) {
+  if (
+    clicked.nodeName === 'SECTION' || 
+    clicked === previousTarget ||
+    clicked.parentNode.classList.contains('selected')
+  ) {
     return;
   }
 
@@ -106,21 +119,23 @@ grid.addEventListener('click', function (event) {
     count++;
     if (count === 1) {
       // Assign first guess
-      firstGuess = clicked.dataset.name;
-      clicked.classList.add('selected');
+      firstGuess = clicked.parentNode.dataset.name;
+      console.log(firstGuess);
+      clicked.parentNode.classList.add('selected');
     } else {
       // Assign second guess
-      secondGuess = clicked.dataset.name;
-      clicked.classList.add('selected');
+      secondGuess = clicked.parentNode.dataset.name;
+      console.log(secondGuess);
+      clicked.parentNode.classList.add('selected');
     }
     // If both guesses are not empty...
     if (firstGuess !== '' && secondGuess !== '') {
       // and the first guess matches the second match...
       if (firstGuess === secondGuess) {
-        match();
-        resetGuesses();
+        setTimeout(match, delay);
+        setTimeout(resetGuesses, delay);
       } else {
-        resetGuesses();
+        setTimeout(resetGuesses, delay);
       }
     }
     // Set previous target to clicked
